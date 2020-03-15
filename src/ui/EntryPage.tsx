89654d1f504acc87b194d31a6c2a58056c7e0e54
Jan Beckschewe <17655375+JanBeckschewe@users.noreply.github.com>
2020-03-16 00:24:04 +0100

    Add gitlab ci for gitlab pages

--- src/ui/EntryPage.tsx
+++ src/ui/EntryPage.tsx
@@ -5,6 +5,8 @@ const EntryPage = () =>
   <div>
     <Link to="/sing/dvgZkm1xWPE">Viva la Vida</Link>
     <Link to="/sing/uSD4vsh1zDA">I gotta feeling</Link>
+    <Link to="/sing/tbNlMtqrYS0">I'm gonna be (500 Miles)</Link>
+    <Link to="/sing/eVTXPUF4Oz4">In the end</Link>
   </div>;
 
 export default EntryPage;
