4ae991562c752d85a16526bc407e3d70453a0e00
Jan Beckschewe <17655375+JanBeckschewe@users.noreply.github.com>
2020-04-08 23:01:27 +0200

    Add footer for join pin

--- src/pages/JoinPlayerPage.jsx
+++ src/pages/JoinPlayerPage.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
-import { Redirect } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const JoinPlayerPage = () => {
 
-  const [partyId, setPartyId] = useState(0);
-  const [redirect, setRedirect] = useState(false);
+  const [partyId, setPartyId] = useState("");
+  const history = useHistory();
 
   const handleChange = event => {
     setPartyId(event.target.value);
   };
 
   const handleSubmit = event => {
-    setRedirect(true);
+    event.preventDefault();
+    history.push(`/mic/${partyId}`);
   };
 
   return (
@@ -21,10 +22,8 @@ const JoinPlayerPage = () => {
         <TextField label="Party ID" type="number" variant="outlined" value={partyId} onChange={handleChange}/>
         <Button type="submit">Join</Button>
       </form>
-
-      {redirect && <Redirect to={`/mic/${partyId}`}/>}
     </div>
-  )
+  );
 };
 
 export default JoinPlayerPage;
