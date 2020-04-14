5db67d7ed314d446400cb9963d26e708d7f5ad52
Jan Beckschewe <17655375+JanBeckschewe@users.noreply.github.com>
2020-04-15 00:56:52 +0200

    Add FlexSearch as backend search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,55 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { apiDomain } from "../GlobalConsts";
-import lunr from "lunr";
 import { TextField, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [songTitles, setSongTitles] = useState([]);
   const [songSearchItemsToShow, setSongSearchItemsToShow] = useState([]);
-  const [idx, setIdx] = useState(null);
 
-  useEffect(() => {
-      (async () => {
-        const resp = await fetch(`https://${apiDomain}/songs`);
-        const jsonObj = await resp.json();
-
-        setSongTitles(jsonObj.data);
-
-        let idx = lunr(function () {
-          this.field("title");
-          this.field("artist");
-
-          for (let [i, item] of jsonObj.data.entries()) {
-            this.add({ ...item, id: i });
-          }
-        });
-
-        setIdx(idx);
-      })();
-    },
-    []);
-
-  const handleSearchTermChange = event => {
+  const handleSearchTermChange = async event => {
     let newTerm = event.target.value;
     setSearchTerm(newTerm);
-    if (newTerm.length >= 2) {
-      let searchResult = idx.search(newTerm);
-      const newItems = searchResult
-        .map(e => songTitles[e.ref])
-        .slice(0, 50);
-      setSongSearchItemsToShow(newItems);
-    } else {
-      setSongSearchItemsToShow([]);
-    }
+
+    let resp = await fetch(`https://${apiDomain}/search/${newTerm}`)
+    let jsonObj = await resp.json();
+
+    setSongSearchItemsToShow(jsonObj.data);
   };
 
   return (
     <div>
       <TextField variant="outlined" value={searchTerm} onChange={handleSearchTermChange}/>
       {songSearchItemsToShow.map((e, i) => {
-        let dashTitle = `${e.artist.replace(/[\W]+/g,"_")}--${e.title.replace(/[\W ]+/g,"_")}`
+        let dashTitle = `${e.artist.replace(/[\W]+/g, "_")}--${e.title.replace(/[\W ]+/g, "_")}`;
 
         return (
           <div key={i}>
