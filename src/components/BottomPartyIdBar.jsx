3753c66c115631ac0c63652136a65caa54fa0cfb
Jan Beckschewe <17655375+jan-be@users.noreply.github.com>
2022-04-16 23:51:08 +0200

    Add Fullscreen

--- src/components/BottomPartyIdBar.jsx
+++ src/components/BottomPartyIdBar.jsx
@@ -13,20 +13,24 @@ const BottomPartyIdBar = ({ partyId, setPartyId, songId, gapData }) => {
     <AppBar position="static" color="primary">
       <Toolbar sx={{ fontSize: 20 }}>
         <Grid container alignItems="center">
-          <Grid item xs={4}>
+          <Grid item xs={3}>
             <Link color="#FFF" underline="none" component={RouterLink} to="/">
               <MyIcon width="16" height="16"/>
               &nbsp;{window.location.hostname}
             </Link>
           </Grid>
 
-          <Grid item xs={4} sx={{ textAlign: "center" }}>
+          <Grid item xs={3} sx={{ textAlign: "center" }}>
             <GapCorrector songId={songId} gapData={gapData}/>
+          </Grid>
+
+          <Grid item xs={3}>
+            <Button variant="outlined" color="secondary" onClick={() => document.documentElement.requestFullscreen()}>Fullscreen</Button>
           </Grid>
 
           {partyId
             ?
-            <Grid item xs={4} sx={{ textAlign: "right" }}>Game PIN: <b>{partyId}</b></Grid>
+            <Grid item xs={3} sx={{ textAlign: "right" }}>Game PIN: <b>{partyId}</b></Grid>
             :
             <Button sx={{ textAlign: "right" }} color="secondary" variant="outlined"
                     onClick={() => setPartyId(getRandInt(1e5, 1e6))}>Start Party</Button>
